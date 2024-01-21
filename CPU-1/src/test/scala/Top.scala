7dcf1db2c04c6e3ac17564f74fc2aa2d4310cc01
Astalisks <astalisksit@gmail.com>
2024-01-21 17:02:45 +0900

    CPU-1が動作するようになった

--- CPU-1/src/test/scala/Top.scala
+++ CPU-1/src/test/scala/Top.scala
@@ -7,15 +7,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class TopTest extends AnyFlatSpec with ChiselScalatestTester {
       it must "runs Top" in { test(new Adder).withAnnotations(Seq(WriteFstAnnotation)) { c =>
+        // 通常のテスト
         c.io.a.poke(3.U)
         c.io.b.poke(5.U)
         c.clock.step(1)
         c.io.out.expect(8.U)
-        // println("Result is: " + c.io.out.peek().toString) // なぜか出力されない
+        // オーバーフローしたときのテスト
         c.io.a.poke(250.U)
         c.io.b.poke(10.U)
         c.clock.step(1)
         c.io.out.expect(4.U)
-        // println("Result is: " + c.io.out.peek().toString)
       } }
 }
--- CPU-1/test_run_dir/must_runs_Top/Adder.sv
+++
@@ -1,9 +0,0 @@
-module Adder(
-  input        clock,
-  input        reset,
-  input  [7:0] io_a,
-  input  [7:0] io_b,
-  output [7:0] io_out
-);
-  assign io_out = io_a + io_b; // @[Adder.scala 12:18]
-endmodule
