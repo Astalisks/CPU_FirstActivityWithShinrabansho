2d945ba8544c51a41874e05d4698f75bc1007b78
Astalisks <astalisksit@gmail.com>
2024-03-31 22:02:52 +0900

    コメント削除

--- CPU-2/src/test/scala/Top.scala
+++ CPU-2/src/test/scala/Top.scala
@@ -14,32 +14,10 @@ class TopTest extends AnyFlatSpec with ChiselScalatestTester {
         dut.clock.step(1)
         dut.io.out.expect(8.U)
         // オーバーフローしたときのテスト（今回はエラーになる）
-        // dut.io.a.poke(2147483647.U)
-        // dut.io.b.poke(10.U)
-        // dut.io.command.poke(1.U)
-        // dut.clock.step(1)
-        // dut.io.out.expect(9.U)
+        dut.io.a.poke(2147483647.U)
+        dut.io.b.poke(10.U)
+        dut.io.command.poke(1.U)
+        dut.clock.step(1)
+        dut.io.out.expect(9.U)
       } }
 }
-
-// package core
-
-// import chisel3._
-// import chisel3.iotesters._
-
-// class TopTest(dut: Alu) extends PeekPokeTester(dut) {
-//     poke(dut.io.a, 3.U)
-//     poke(dut.io.b, 5.U)
-//     step(1)
-//     println("Result is: " + peek(dut.io.out).toString)
-//     poke(dut.io.a, 250.U)
-//     poke(dut.io.b, 10.U)
-//     step(1)
-//     println("Result is: " + peek(dut.io.out).toString)
-// }
-
-// object TopTest extends App {
-//     chisel3.iotesters.Driver(() => new Alu()) { c =>
-//         new TopTest(c)
-//     }
-// }
